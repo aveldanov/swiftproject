1fea98eaa2219f3a9825a1e5d60d442abfb69ea1
Anton Veldanov <anton.veldanov@gmail.com>
2019-11-17 13:50:01 -0800

    IOS Apps

---
+++ I Am Poor/I Am PoorTests/I_Am_PoorTests.swift
@@ -0,0 +1,34 @@
+//
+//  I_Am_PoorTests.swift
+//  I Am PoorTests
+//
+//  Created by Veldanov, Anton on 11/17/19.
+//  Copyright © 2019 Veldanov, Anton. All rights reserved.
+//
+
+import XCTest
+@testable import I_Am_Poor
+
+class I_Am_PoorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
