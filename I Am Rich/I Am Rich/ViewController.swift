cb8a79f662735975f0ccafc206a8e21664eb23fd
Anton Veldanov <anton.veldanov@gmail.com>
2019-11-16 23:04:24 -0800

    first commit

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Veldanov, Anton on 11/16/19.
+//  Copyright © 2019 Veldanov, Anton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
