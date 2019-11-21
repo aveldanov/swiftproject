643f397601683a293a3ef2b3cc5cff8a4d02e6e7
Anton Veldanov <anton.veldanov@gmail.com>
2019-11-21 15:11:01 -0800

    IOS Apps

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,9 +3,13 @@ func loveCalculator(){
     
     let lovescore = Int.random(in: 0...100)
 
-    if lovescore==100{
+    if lovescore > 80{
         print("You love yourself as Kanye loves Kanye")
+    }
+    if lovescore > 40{
+        print("Coke and Mentos")
     }else{
+        
         print("You are alone")
     }
     
