3a7b529d67fa70b3a380dd92c119d0aad813f085
Anton Veldanov <anton.veldanov@gmail.com>
2019-11-25 23:05:50 -0800

    Comments

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,10 +2,12 @@ struct Town {
     let name = "AntonLand"
     var citizens = ["cit1", "cit2"]
     var resources = ["Grain":100, "Ore":42, "Wool": 75]
+    
+    
+    func fortify(){
+        print("Defenses increased")
+    }
 }
 
 
-
-var myTown = Town()
-
-print(myTown.citizens)
+//
