743548863990f74efe5a919fe09dcf4989032cd0
Anton Veldanov <anton.veldanov@gmail.com>
2019-12-29 09:46:59 -0800

    IOS Apps

--- ClassesDemo/ClassesDemo/Enemy.swift
+++ ClassesDemo/ClassesDemo/Enemy.swift
@@ -1,9 +1,20 @@
 
+// Struct is immutable
 
-
-class Enemy{
-    var health = 100
-    var attackStrength = 10
+struct Enemy{
+    var health: Int
+    var attackStrength: Int
+    
+    init(health:Int, attackStrength:Int) {
+        self.health = health
+        self.attackStrength = attackStrength
+    }
+    
+    mutating func takeDamage(amount: Int){
+        
+        health = health - amount
+    }
+    
     
     func move(){
         
