c3fe4b5d97d3405aef969f8e300d33a5aea05f84
Anton Veldanov <anton.veldanov@gmail.com>
2020-01-06 15:04:20 -0800

    IOS Apps

---
+++ Protocols Demo/Protocols Demo/main.swift
@@ -0,0 +1,70 @@
+
+
+protocol CanFly {
+    func fly()
+}
+
+
+
+class Bird{
+    
+    var isFemale = true
+    
+    func layEgg(){
+        if isFemale{
+            print("LAYED AN EGG")
+        }
+    }
+    
+//    func fly(){
+//        print("BIRD FLYING")
+//    }
+    
+    
+}
+
+
+class Eagle: Bird,CanFly {
+    func fly() {
+        print("EAGLE FLIES")
+    }
+    
+    
+    func soar(){
+        print("EAGLE SOARS")
+    }
+}
+
+
+
+
+
+class Penguin: Bird {
+    func swim(){
+        print("PENGUIN SWIM")
+    }
+}
+
+
+struct FlyingMuseum {
+    func flyingDemos(flyingObject: CanFly){
+        flyingObject.fly()
+    }
+}
+
+
+struct Airplane: CanFly {
+    
+     func fly() {
+        print("PLANE IS FLYING")
+    }
+    
+}
+
+let myEagle = Eagle()
+let myPenguin = Penguin()
+let  museum = FlyingMuseum()
+let myPlane = Airplane()
+
+museum.flyingDemos(flyingObject: myEagle)
+museum.flyingDemos(flyingObject: myPlane)
