3ae874ef0d9d389fc8f7430fe3fc9504ba76c91a
Anton Veldanov <anton.veldanov@gmail.com>
2020-01-06 16:19:52 -0800

    IOS Apps

--- Protocols and Delegates/Protocols and Delegates/main.swift
+++ Protocols and Delegates/Protocols and Delegates/main.swift
@@ -4,5 +4,33 @@ protocol AdvancedLifeSupport {
 
 
 class EmergencyCallHandler {
-    var delegate: AdvancedLifeSupport
+    // does not matter who is a delegate (INTERN)
+    var delegate: AdvancedLifeSupport?
+    
+    func assessSituation(){
+        print("Call Handler: Can you tell what happend?")
+    }
+    
+    func medicalEmergnecy(){
+        delegate?.performCPR()
+    }
 }
+
+
+struct Paramedic: AdvancedLifeSupport {
+    // initial input
+    init(handler: EmergencyCallHandler){
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("Paramedic is saving life")
+    }
+    
+}
+
+let callHandler1 = EmergencyCallHandler()
+let paramedicOnCall1 = Paramedic(handler: callHandler1)
+
+callHandler1.assessSituation()
+
