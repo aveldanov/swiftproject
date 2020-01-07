11dd3bc0bf8edc3fd7556af2d301de69f4ba2f0c
Anton Veldanov <anton.veldanov@gmail.com>
2020-01-06 16:01:36 -0800

    IOS Apps

--- Protocols and Delegates/Protocols and Delegates/main.swift
+++ Protocols and Delegates/Protocols and Delegates/main.swift
@@ -1,12 +1,3 @@
-//
-//  main.swift
-//  Protocols and Delegates
-//
-//  Created by Veldanov, Anton on 1/6/20.
-//  Copyright © 2020 Anton Veldanov. All rights reserved.
-//
-
-import Foundation
-
-print("Hello, World!")
-
+protocol AdvancedLifeSupport {
+    func performCPR()
+}
