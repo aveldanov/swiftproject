2f559a4837516a628abbf39e4fb37ebb8dee1605
Anton Veldanov <anton.veldanov@gmail.com>
2020-01-06 16:35:42 -0800

    IOS Apps

--- Protocols and Delegates/Protocols and Delegates/main.swift
+++ Protocols and Delegates/Protocols and Delegates/main.swift
@@ -12,25 +12,69 @@ class EmergencyCallHandler {
     }
     
     func medicalEmergnecy(){
+//        print("DELEGATE IS ASSGINED \(delegate)")
         delegate?.performCPR()
     }
 }
+
+
 
 
 struct Paramedic: AdvancedLifeSupport {
     // initial input
     init(handler: EmergencyCallHandler){
         handler.delegate = self
+//        print("HANDLER: \(handler)")
     }
     
     func performCPR() {
-        print("Paramedic is saving life")
+        print("Paramedic: is saving life")
     }
     
 }
 
+class Doctor:AdvancedLifeSupport {
+    
+    init(handler: EmergencyCallHandler){
+        handler.delegate = self
+//        print("HANDLER: \(handler)")
+    }
+    
+    
+    func performCPR() {
+        print("Doctor: is saving life")
+        
+    }
+    
+    func useStepescope(){
+        print("DOCTOR: using stephescope")
+    }
+    
+    
+}
+
+
+
+class Surgeon: Doctor{
+    override func performCPR() {
+        super.performCPR()
+        print("SURGEON: do surgery")
+    }
+    
+    func useScalpel(){
+        print("SURGEON: uses scalpel")
+    }
+    
+    
+}
+
+
 let callHandler1 = EmergencyCallHandler()
-let paramedicOnCall1 = Paramedic(handler: callHandler1)
+//let paramedicOnCall1 = Paramedic(handler: callHandler1)
+
+let surgeonOnCall1 = Surgeon(handler: callHandler1)
+
 
 callHandler1.assessSituation()
+callHandler1.medicalEmergnecy()
 
