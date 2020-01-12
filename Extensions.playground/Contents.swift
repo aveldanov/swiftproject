76c2599c7f551a80b44a65e8430ad8ab22f79023
Anton Veldanov <anton.veldanov@gmail.com>
2020-01-11 21:31:25 -0800

    IOS Apps

---
+++ Extensions.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+extension Double {
+    
+    
+    
+    
+}
+
+
+var myDouble = 3.14159
+
+//let myRoundedDouble = String(format: "%.1f", myDouble)
+
+
+myDouble.round(to: 3)
+
+//print(myRoundedDouble)
