c1de89ef8b0f6abe9de09c58ad17faa5d309a53b
Anton Veldanov <anton.veldanov@gmail.com>
2020-01-12 20:00:27 -0800

    IOS Apps

--- Extensions.playground/Contents.swift
+++ Extensions.playground/Contents.swift
@@ -2,7 +2,15 @@ import UIKit
 
 extension Double {
     
-    
+    func round(to places: Int) -> Double{
+        let precisionNumber = pow(10, Double(places))
+        var n = self  // Current Value of the Double ->  3.14159
+        n = n*precisionNumber
+        n.round()
+        n = n/precisionNumber
+        
+        return n
+    }
     
     
 }
@@ -10,9 +18,12 @@ extension Double {
 
 var myDouble = 3.14159
 
+//myDouble = myDouble*1000
+//myDouble.round()
+//myDouble = myDouble/1000
 //let myRoundedDouble = String(format: "%.1f", myDouble)
 
 
-myDouble.round(to: 3)
+myDouble.round(to: 4)
 
 //print(myRoundedDouble)
