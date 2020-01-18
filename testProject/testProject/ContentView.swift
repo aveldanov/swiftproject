afe1953ea25b11a39afb4c77c439342ce9d1dbda
Anton Veldanov <anton.veldanov@gmail.com>
2020-01-17 19:28:47 -0800

    IOS Apps

---
+++ testProject/testProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testProject
+//
+//  Created by Veldanov, Anton on 1/15/20.
+//  Copyright © 2020 Anton Veldanov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testProjectDelegates/testProjectDelegates/Controllers/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  testProjectDelegates
+//
+//  Created by Veldanov, Anton on 1/15/20.
+//  Copyright © 2020 Anton Veldanov. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    
+    @IBOutlet weak var chooseButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        
+    }
+
+    
+    
+    
+    
+    @IBAction func chooseButtonClicked(_ sender: UIButton) {
+        
+        
+        let selectVC = 
+        
+    }
+    
+
+}
+
